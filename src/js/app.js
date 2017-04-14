4bff3ec51a16633f96c7dd4495ea10302ebefc50
Casey Ross <caseyross003@gmail.com>
2017-04-13 21:04:59 -0500

    refactor: Add Gulp

---
+++ src/js/app.js
@@ -0,0 +1,11 @@
+function openNav() {
+    document.getElementById("search").style.width = "280px";
+    document.getElementById("map").style.marginLeft = "280px";
+    document.getElementById("map").style.width = "calc(100% - 300px)";
+}
+
+function closeNav() {
+    document.getElementById("search").style.width = "0";
+    document.getElementById("map").style.marginLeft = "20px";
+    document.getElementById("map").style.width = "calc(100% - 40px)";
+}
--- static/js/map.js
+++
@@ -1,8 +0,0 @@
-var map;
-function initMap() {
-   // Constructor creates a new map - only center and zoom are required.
-   map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: 30.32500, lng: -97.65000},
-      zoom: 13
-   });
-}
